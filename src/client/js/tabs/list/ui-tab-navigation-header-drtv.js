b22d67b69d2397bfb3f3a1d64d8b7e7069b8bf08
aviad cohen <aviad202000@yahoo.com>
2015-07-10 19:34:29 +0300

    implement selection of tab navigation headers

--- src/client/js/tabs/list/ui-tab-navigation-header-drtv.js
+++ src/client/js/tabs/list/ui-tab-navigation-header-drtv.js
@@ -7,6 +7,14 @@ angular.module('tabs').directive('uiTabNavigationHeader', [function () {
         templateUrl: 'js/tabs/list/ui-tab-navigation-header-tmpl.html',
         link: function (scope) {
 
+            // if this is the first tab - make it active
+            if (scope.$index === 0) {
+
+                // the element is not yet interpolated here, by now, from some reason
+                scope.$applyAsync(function () {
+                    scope.selectTab(0);
+                });
+            }
         }
     };
 }]);
--- src/client/js/tabs/list/ui-tabs-list-drtv.js
+++ src/client/js/tabs/list/ui-tabs-list-drtv.js
@@ -7,8 +7,21 @@ angular.module('tabs').directive('uiTabsList', ['RegisteredTabsList', function (
         templateUrl: 'js/tabs/list/ui-tabs-list-tmpl.html',
         scope: true,
         link: function (scope, element) {
+
+            var selectedTab = 0;
+
             scope.tabsNames = RegisteredTabsList.getTabsNames();
 
+            scope.selectTab = function selectTab(tabIndex) {
+
+                var selectedElement = element.find('#item-' + selectedTab);
+                selectedElement.removeClass('active');
+
+                selectedTab = tabIndex;
+
+                selectedElement = element.find('#item-' + tabIndex);
+                selectedElement.addClass('active');
+            };
         }
     };
 }]);
