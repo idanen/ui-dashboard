814bf04e2e3be7325f6132dc4e28bbc5a3c4bf89
Fadi Matar <fadi.matar@hp.com>
2015-07-23 10:28:58 +0300

    info tab (scopes,watches..)

--- src/client/js/modules-declaration.js
+++ src/client/js/modules-declaration.js
@@ -1,2 +1,16 @@
 angular.module('ui', ['tabs']);
 angular.module('tabs', []);
+
+
+angular.module('tabs').controller('infoCtrl',['$scope',function($scope){
+    $scope.currentStatue = "infoImg";
+    $scope.status = function(){
+        if($scope.currentStatue == "" || $scope.currentStatue == "infoImg"){
+            $scope.currentStatue="enlarge";
+        }else{
+            $scope.currentStatue="infoImg";
+        }
+
+        return $scope.currentStatue;
+    }
+}]);
