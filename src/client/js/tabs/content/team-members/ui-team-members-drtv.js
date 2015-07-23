ef4dafcf2e4ee53bf5704b51ce6363cfa26f4cd9
Mazit Bentora <mazit.bentora@hp.com>
2015-07-23 14:27:34 +0300

    edit team member

--- src/client/js/tabs/content/team-members/ui-team-members-drtv.js
+++ src/client/js/tabs/content/team-members/ui-team-members-drtv.js
@@ -2,22 +2,37 @@
     'use strict';
 
     angular.module('tabs').directive('uiTeamMembers', ['TeamMembersService', function (TeamMembersService) {
-            return {
-                restrict: 'E',
-                controllerAs: 'teamMembersCtrl',
-                controller: [function () {
-                    var ctrl = this;
+        return {
+            restrict: 'E',
+            controllerAs: 'teamMembersCtrl',
+            controller: [function () {
+                var ctrl = this;
+                this.members = TeamMembersService.getMembers();
+                this.addMember = function (member) {
+                    TeamMembersService.addMember(member);
+                };
+                this.setEditMember = function (member) {
+                    ctrl.editMember = member;
+                };
+                this.saveMember = function () {
+                    TeamMembersService.saveMemberInfo(ctrl.editMember);
+                };
+                this.cancelEditMember = function () {
+                    TeamMembersService.getOrigMember(ctrl.editMember.$id).then(function (data) {
+                        ctrl.editMember.fname = data.fname;
+                        ctrl.editMember.lname = data.lname;
+                        ctrl.editMember.compName = data.compName;
+                        ctrl.editMember.email = data.email;
+                        ctrl.editMember.img = data.img;
+                    });
+                };
+            }],
+            templateUrl: 'js/tabs/content/team-members/ui-team-members-tmpl.html',
+            link: function ($scope, element, attributes, controller) {
 
-                }],
-                templateUrl: 'js/tabs/content/team-members/ui-team-members-tmpl.html',
-                link: function ($scope, element, attributes, controller) {
-                    $scope.members = TeamMembersService.members;
-                    $scope.addMember = function (member) {
-                        TeamMembersService.addMember(member);
-                    };
-                }
-            };
-        }])
+            }
+        };
+    }])
     ;
 })
 (window.angular);
