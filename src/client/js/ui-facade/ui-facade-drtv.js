ecad7bc6e62a981206efdc173d4c5bec98299543
Idan Entin <idan.entin@hp.com>
2015-07-26 19:15:35 +0300

    Layouting

--- src/client/js/ui-facade/ui-facade-drtv.js
+++ src/client/js/ui-facade/ui-facade-drtv.js
@@ -1,17 +1,36 @@
 (function (angular) {
     'use strict';
 
-    angular.module('ui').directive('uiFacade', ['RegisteredTabsList', function (RegisteredTabsList) {
-        return {
-            restrict: 'E',
-            templateUrl: '/js/ui-facade/ui-facade-tmpl.html',
-            link: function (scope, element) {
+    angular.module('ui')
+        .directive('uiFacade', ['RegisteredTabsList', function (RegisteredTabsList) {
+            return {
+                restrict: 'E',
+                templateUrl: '/js/ui-facade/ui-facade-tmpl.html',
+                controller: 'UiFacadeCtrl',
+                controllerAs: 'facade',
+                link: function (scope, element) {
 
-                scope.appplySelectedTabContent = function applySelectedTabContent(tabId) {
-                    var tabUrlContent = RegisteredTabsList.getTabContent(tabId);
-                    scope.selectedTabContent = tabUrlContent;
-                };
+                    scope.appplySelectedTabContent = function applySelectedTabContent(tabId) {
+                        var tabUrlContent = RegisteredTabsList.getTabContent(tabId);
+                        scope.selectedTabContent = tabUrlContent;
+                    };
+                }
+            };
+        }])
+        .controller('UiFacadeCtrl', UiFacadeController);
+
+    function UiFacadeController() {
+        this.widgets = [
+            {
+                title: 'Push Queue',
+                contentUrl: 'js/tabs/list/tabs-content-templates/tab-content-ui-push-queue-tmpl.html',
+                columns: 6
+            },
+            {
+                title: 'Team Members',
+                contentUrl: 'js/tabs/list/tabs-content-templates/tab-content-ui-team-members-tmpl.html',
+                columns: 6
             }
-        };
-    }]);
+        ];
+    }
 })(window.angular);
