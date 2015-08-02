ed51d2a542e9a97b53ab46923924f10abb61cd83
Mazit Bentora <mazit.bentora@hp.com>
2015-08-02 14:38:08 +0300

    Queue is empty tag fix

--- src/client/js/tabs/content/push-queue/ui-push-queue-drtv.js
+++ src/client/js/tabs/content/push-queue/ui-push-queue-drtv.js
@@ -8,24 +8,15 @@
                 controllerAs: 'pushQueueCtrl',
                 controller: [function () {
 
-                    var ctrl = this;
                     this.queue = PushQueueService.getQueue();
                     this.members = TeamMembersService.getMembers();
-                    this.empty = '';
 
                     this.addToQueue = function () {
                         PushQueueService.addToQueue(this.selected.memberId);
-                        ctrl.empty = '';
                     };
 
                     this.removeFromQueue = function (id) {
-                        PushQueueService.removeFromQueue(id).then(function (isEmpty) {
-                            if (isEmpty) {
-                                ctrl.empty = 'Queue is Empty';
-                            }
-                        }).catch(function () {
-                            console.log();
-                        });
+                        PushQueueService.removeFromQueue(id);
                     };
 
                     this.getFirstName = function (memberId) {
@@ -40,16 +31,9 @@
                         PushQueueService.fireNotification();
                     };
                 }],
-                templateUrl: 'js/tabs/content/push-queue/ui-push-queue-tmpl.html',
-                link: function ($scope, element, attributes, controller) {
-                    controller.queue.$loaded(function () {
-                        if (controller.queue.length === 0) {
-                            controller.empty = 'Queue is Empty';
-                        }
-                    });
-                }
+                templateUrl: 'js/tabs/content/push-queue/ui-push-queue-tmpl.html'
             };
-        }])
-    ;
+        }
+    ]);
 })
 (window.angular);
