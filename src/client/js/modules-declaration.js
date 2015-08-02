20744022840b4611c42ecf16200bc0b5b9937b23
Idan Entin <idan.entin@hp.com>
2015-08-02 13:57:38 +0300

    Change server name

--- src/client/js/modules-declaration.js
+++ src/client/js/modules-declaration.js
@@ -1,5 +1,5 @@
 angular.module('tabs', ['firebase', 'ngSanitize', 'ui.select']).constant('ENV', {
-    HOST: 'myd-vm01818.hpswlabs.adapps.hp.com/',
+    HOST: 'myd-vm01818.hpswlabs.adapps.hp.com',
     PORT: '4000'
 });
 angular.module('ui', ['tabs']);
