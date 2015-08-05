82441ba60314f09caddad371679f388538be3bba
Idan Entin <idan.entin@hp.com>
2015-08-05 17:29:18 +0300

    Ladda config

--- src/client/js/modules-declaration.js
+++ src/client/js/modules-declaration.js
@@ -1,14 +1,21 @@
-angular.module('tabs', ['firebase', 'ngSanitize', 'ui.select', 'angular-ladda'])
-    .constant('ENV', {
-        HOST: 'localhost',
-        PORT: '4000'
-    })/*
-    .config(['$stateProvider', '$locationProvider'/!*, '$urlRouterProvider'*!/, function config($stateProvider, $locationProvider/!*, $urlRouterProvider*!/) {
-        $locationProvider.html5Mode(true);
+(function () {
+    'use strict';
 
-        $stateProvider.state('members', {
-            templateUrl: 'js/tabs/content/team-members/ui-team-members-tmpl.html',
-            controller: 'TeamMembersCtrl as teamMembersCtrl'
-        });
-    }])*/;
-angular.module('ui', ['tabs']);
+    angular.module('tabs', ['firebase', 'ngSanitize', 'ui.select', 'angular-ladda'])
+        .constant('ENV', {
+            HOST: 'localhost',
+            PORT: '4000'
+        }).config(['laddaProvider', function (laddaProvider) {
+            laddaProvider.setOption({
+                style: 'expand-right'
+            });
+        }])/*.config(['$stateProvider', '$locationProvider'/!*, '$urlRouterProvider'*!/, function config($stateProvider, $locationProvider/!*, $urlRouterProvider*!/) {
+             $locationProvider.html5Mode(true);
+
+             $stateProvider.state('members', {
+             templateUrl: 'js/tabs/content/team-members/ui-team-members-tmpl.html',
+             controller: 'TeamMembersCtrl as teamMembersCtrl'
+         });
+     }])*/;
+    angular.module('ui', ['tabs']);
+})();
