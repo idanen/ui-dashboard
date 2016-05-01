8d7d28707dbf30746ca48f47143b29c964f04bc8
Idan Entin <idanen@gmail.com>
2016-05-01 15:03:52 +0300

    Move template to a template file

--- src/client/js/tabs/content/compare/build-selector-drtv.js
+++ src/client/js/tabs/content/compare/build-selector-drtv.js
@@ -9,26 +9,12 @@
           selected: '<',
           onChange: '&'
         },
-        template: `
-          <div>
-            <label>Select build and number</label>
-            <select name="selectedBuildName"
-                    ng-model="$ctrl.name"
-                    ng-change="$ctrl.onChange({prop: 'name', value: $ctrl.name})">
-              <option ng-repeat="(buildId, build) in $ctrl.builds track by buildId" value="{{ buildId }}" ng-bind="buildId"></option>
-            </select>
-            <span>#</span>
-            <select name="selectedBuildNumber"
-                    ng-model="$ctrl.number"
-                    ng-change="$ctrl.onChange({prop: 'number', value: $ctrl.number})">
-              <option ng-repeat="(buildNumber, buildResult) in $ctrl.builds[$ctrl.selected.name].builds track by buildNumber" value="{{ buildNumber }}" ng-bind="buildNumber"></option>
-            </select>
-          </div>
-        `
+        templateUrl: 'js/tabs/content/compare/build-selector-tmpl.html'
       });
 
-  BuildSelectorController.$inject = [];
-  function BuildSelectorController() {
+  BuildSelectorController.$inject = ['$element'];
+  function BuildSelectorController($element) {
+    this.$element = $element;
   }
 
   BuildSelectorController.prototype = {
@@ -49,6 +35,8 @@
         this.name = buildId;
         this.number = buildNumber;
       }
-    }
+    },
+    $postLink: angular.noop,
+    $onDestroy: angular.noop
   };
 }());
