9d8af1a7111028f4dc39feaec1240849a35c023d
Idan Entin <idanen@gmail.com>
2016-05-30 17:45:24 +0300

    [fix]: Fix small errors

--- src/client/js/modules-declaration.js
+++ src/client/js/modules-declaration.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
 
+    angular.module('ci-site.filters', []);
     angular.module('ci-site', ['firebase', 'ngAnimate', 'ngSanitize', 'ngResource', 'ui.router', 'ui.select', 'ui.bootstrap', 'angular-ladda', 'ngclipboard', 'ci-site.filters', 'collapsiblePanel'])
         .constant('ENV', {
             HOST: 'myd-vm08383.hpswlabs.adapps.hp.com',
