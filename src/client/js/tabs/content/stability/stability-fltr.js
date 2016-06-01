e67b207241e08c2e71ba2ee663de7f8b8e1124a5
Idan Entin <idanen@gmail.com>
2016-06-01 15:09:13 +0300

    [enhancement stability]: Show report of last failed in any of the last `n` builds (Issue #7)

--- src/client/js/tabs/content/stability/stability-fltr.js
+++ src/client/js/tabs/content/stability/stability-fltr.js
@@ -4,6 +4,8 @@
   angular.module('ci-site')
       .filter('stabilityFilter', function () {
         return function (tests, limit) {
+          let idxToRemove = [];
+
           if (!tests || !Array.isArray(tests) || isNaN(limit) || limit < 0 || limit > 1) {
             return tests;
           }
@@ -25,6 +27,11 @@
               test.methods = test.methods.slice(idx, idx + 1);
               delete test.results[testToFilter];
             });
+
+            if (test.methods.length === 0) {
+              // TODO idanen: remove nulls in the end (use [].filter(Boolean))
+              return null;
+            }
 
             return test;
           });
