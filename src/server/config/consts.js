bdbb042c14c7b45dd435294d3e520c22f58d13d4
Idan Entin <idanen@gmail.com>
2016-08-03 17:12:38 +0300

    Fix mongo domain

--- src/server/config/consts.js
+++ src/server/config/consts.js
@@ -2,7 +2,7 @@ module.exports = {
   JENKINS_JOB_URL: 'http://jenkins.hpe.guru:8080/jenkins/job/',
   JENKINS_JSON_SUFFIX: 'api/json',
   JENKINS_CREDENTIALS: 'aWRhbi5lbnRpbkBocGUuY29tOjJ3c3gjRURD',
-  TESTS_MONGO_CONNECTION: 'mongodb://myd-vm00058.hpswlabs.adapps.hp.com:27017/failsafereports',
+  TESTS_MONGO_CONNECTION: 'mongodb://myd-vm00058.hpeswlab.net:27017/failsafereports',
   FIREBASE_URL_CI_JOBS: 'https://boiling-inferno-9766.firebaseio.com/allJobs',
   FIREBASE_URL_CI_STATUS: 'https://boiling-inferno-9766.firebaseio.com/ciStatus',
   FIREBASE_REST_SUFFIX: '.json',
